9c5e223182d981095d425aa968c5fa2431d2175e
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2024-11-07 18:18:56 +0100

    Add env component

--- src/UpsertProductCommand.php
+++ src/UpsertProductCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PimUpsertProductTool;
 
+use Akeneo\Pim\ApiClient\AkeneoPimClientBuilder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,6 +16,19 @@ final class UpsertProductCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Hello world');
+
+        $host = $_ENV['HOST'];
+        $clientId = $_ENV['CLIENT_ID'];
+        $secret = $_ENV['CLIENT_SECRET'];
+        $username = $_ENV['USERNAME'];
+        $password = $_ENV['PASSORD'];
+
+        $builder = new AkeneoPimClientBuilder($host);
+        $client = $builder->buildAuthenticatedByPassword($clientId, $secret, $username, $password);
+
+        $families = $client->getFamilyApi()->all(10);
+
+        $output->writeln(\sprintf('%d families found.', \count(\iterator_to_array($families))));
 
         return Command::SUCCESS;
     }
